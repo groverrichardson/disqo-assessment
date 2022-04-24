13b5ddbf508e16126edff500b55d807e0acc6ca7
Grover Richardson <groverrichardson1987@gmail.com>
2022-04-24 15:07:56 -0700

    Initialized repo.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Home from './components/home';
+import 'tailwindcss/tailwind.css';
+
+function App() {
+    return (
+        <main className="App">
+            <Home />
+        </main>
+    );
+}
+
+export default App;
---
+++ src/components/home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Home() {
+    return <main className="home">Hi there</main>;
+}
+
+export default Home;
