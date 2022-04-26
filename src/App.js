12cd80defc559156fddd021c0c3e793b0aa7efe6
Grover Richardson <groverrichardson1987@gmail.com>
2022-04-25 20:57:30 -0700

    Added note entry section and buttons.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import 'tailwindcss/tailwind.css';
 import NotepadHero from './components/notepad-hero';
+import './styles/tailwind.sass';
 
 function App() {
     return (
         <main className="App flex flex-col justify-center items-center">
-            <div className="app-container flex flex-col justify-center items-center">
+            <div className="app-container flex flex-row flex-wrap justify-start items-start">
                 <NotepadHero />
+                <form className="notes-container flex flex-col justify-center items-start">
+                    <h2 className="my-notes ">My Notes</h2>
+                    <input
+                        type="text"
+                        className="note-title border-2 border-gray-300 p-2 rounded-md mt-2"
+                        placeholder="Enter note title..."
+                    />
+                    <textarea
+                        name="note-input"
+                        id="note-input"
+                        cols="30"
+                        rows="10"
+                        className="border-2 border-gray-300 p-2 rounded-md mt-2"
+                        placeholder="Enter note..."></textarea>
+                    <button className="note-submit bg-green-500 py-2 px-4 mt-2 text-white">
+                        Add
+                    </button>
+                </form>
             </div>
         </main>
     );
--- src/components/notepad-hero.js
+++ src/components/notepad-hero.js
@@ -2,14 +2,25 @@ import React from 'react';
 
 function NotepadHero() {
     return (
-        <div className="NotepadHero">
+        <div className="NotepadHero flex flex-row justify-between items-center w-full">
             <div className="title-container">
                 <h1 className="notepad-title">Notepad Title</h1>
                 <input
                     type="text"
-                    className="notepad-title-input border-2 border-gray-300 p-2 rounded-md mt-2 w-96"
+                    className="notepad-title-input border-2 border-gray-300 p-2 rounded-md mt-2"
                     placeholder="My notepad title..."
                 />
+            </div>
+            <div className="button-group flex flex-row justify-between items-center w-72 max-w-full">
+                <button className="view-stat border-2 border-gray-300 py-2 px-4 rounded-md">
+                    View Stats
+                </button>
+                <button className="save border-2 border-gray-300 py-2 px-4 rounded-md">
+                    Save
+                </button>
+                <button className="Delete border-2 border-gray-300 py-2 px-4 rounded-md">
+                    Delete
+                </button>
             </div>
         </div>
     );
