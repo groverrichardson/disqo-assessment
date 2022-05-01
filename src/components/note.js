592afedebd6c28d6ccf357bae22cd10bbe565de6
Grover Richardson <groverrichardson1987@gmail.com>
2022-05-01 15:26:33 -0700

    Added structure to note component.

--- src/components/note.js
+++ src/components/note.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
 function Note() {
-    return <div className="Note"></div>;
+    return (
+        <div className="Note flex flex-row justify-between items-start ">
+            <div className="note-details-container">
+                <input
+                    type="text"
+                    className="note-title border-2 border-gray-300 p-2 rounded-md mt-2"
+                    placeholder="Note title"
+                />
+                <textarea
+                    name="note-description"
+                    id="note-description"
+                    cols="30"
+                    rows="10"
+                    className="note-description border-2 border-gray-300 p-2 rounded-md mt-2"
+                    placeholder="Note description"></textarea>
+            </div>
+            <button className="delete bg-red-500 text-white py-2 px-3">
+                Delete
+            </button>
+        </div>
+    );
 }
 
 export default Note;
