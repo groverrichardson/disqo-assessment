8b2812ace2b21e6685851076705013e1ffefeb5b
Grover Richardson <groverrichardson1987@gmail.com>
2022-05-01 15:11:35 -0700

    Restructed components to better represent design.

--- src/App.js
+++ src/App.js
@@ -8,24 +8,17 @@ function App() {
         <main className="App flex flex-col justify-center items-center">
             <div className="app-container flex flex-row flex-wrap justify-start items-start">
                 <NotepadHero />
-                <form className="notes-container flex flex-col justify-center items-start">
-                    <h2 className="my-notes ">My Notes</h2>
-                    <input
-                        type="text"
-                        className="note-title border-2 border-gray-300 p-2 rounded-md mt-2"
-                        placeholder="Enter note title..."
-                    />
-                    <textarea
-                        name="note-input"
-                        id="note-input"
-                        cols="30"
-                        rows="10"
-                        className="border-2 border-gray-300 p-2 rounded-md mt-2"
-                        placeholder="Enter note..."></textarea>
-                    <button className="note-submit bg-green-500 py-2 px-4 mt-2 text-white">
-                        Add
+                <div className="button-group flex flex-row justify-between items-center w-72 max-w-full">
+                    <button className="view-stat border-2 border-gray-300 py-2 px-4 rounded-md">
+                        View Stats
                     </button>
-                </form>
+                    <button className="save border-2 border-gray-300 py-2 px-4 rounded-md">
+                        Save
+                    </button>
+                    <button className="Delete border-2 border-gray-300 py-2 px-4 rounded-md">
+                        Delete
+                    </button>
+                </div>
             </div>
         </main>
     );
--- src/components/notepad-hero.js
+++ src/components/notepad-hero.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function NotepadHero() {
     return (
-        <div className="NotepadHero flex flex-row justify-between items-center w-full">
+        <div className="NotepadHero flex flex-col justify-center items-between w-1/2">
             <div className="title-container">
                 <h1 className="notepad-title">Notepad Title</h1>
                 <input
@@ -11,17 +11,24 @@ function NotepadHero() {
                     placeholder="My notepad title..."
                 />
             </div>
-            <div className="button-group flex flex-row justify-between items-center w-72 max-w-full">
-                <button className="view-stat border-2 border-gray-300 py-2 px-4 rounded-md">
-                    View Stats
+            <form className="notes-container flex flex-col justify-center items-start">
+                <h2 className="my-notes ">My Notes</h2>
+                <input
+                    type="text"
+                    className="note-title border-2 border-gray-300 p-2 rounded-md mt-2"
+                    placeholder="Enter note title..."
+                />
+                <textarea
+                    name="note-input"
+                    id="note-input"
+                    cols="30"
+                    rows="10"
+                    className="border-2 border-gray-300 p-2 rounded-md mt-2"
+                    placeholder="Enter note..."></textarea>
+                <button className="note-submit bg-green-500 py-2 px-4 mt-2 text-white">
+                    Add
                 </button>
-                <button className="save border-2 border-gray-300 py-2 px-4 rounded-md">
-                    Save
-                </button>
-                <button className="Delete border-2 border-gray-300 py-2 px-4 rounded-md">
-                    Delete
-                </button>
-            </div>
+            </form>
         </div>
     );
 }
